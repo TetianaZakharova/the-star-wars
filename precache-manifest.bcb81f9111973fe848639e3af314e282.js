1549f3e8d92266bd5d0834b6f818fbdb4692a47a
T Z <kazakova.tanya@gmail.com>
2020-07-16 14:15:05 +0300

    Updates

---
+++ precache-manifest.bcb81f9111973fe848639e3af314e282.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9356124e505901420ac0109f8b69d130",
+    "url": "/the-star-wars/index.html"
+  },
+  {
+    "revision": "1a5daaeef0474f1c45dc",
+    "url": "/the-star-wars/static/css/main.46c560c6.chunk.css"
+  },
+  {
+    "revision": "88fec03b1b62c3c16db5",
+    "url": "/the-star-wars/static/js/2.261834b9.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/the-star-wars/static/js/2.261834b9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1a5daaeef0474f1c45dc",
+    "url": "/the-star-wars/static/js/main.94aec738.chunk.js"
+  },
+  {
+    "revision": "c06aca2ff527df84da23",
+    "url": "/the-star-wars/static/js/runtime-main.152ec49d.js"
+  },
+  {
+    "revision": "9c65d2a650dc3e5bddf8729a9c4c9101",
+    "url": "/the-star-wars/static/media/StarWars.9c65d2a6.png"
+  }
+]);
---
+++ service-worker.js
@@ -0,0 +1,39 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
+
+importScripts(
+  "/the-star-wars/precache-manifest.bcb81f9111973fe848639e3af314e282.js"
+);
+
+self.addEventListener('message', (event) => {
+  if (event.data && event.data.type === 'SKIP_WAITING') {
+    self.skipWaiting();
+  }
+});
+
+workbox.core.clientsClaim();
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
+
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/the-star-wars/index.html"), {
+  
+  blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
+});
